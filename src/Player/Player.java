e4b4e04c455c2cc500211fe905aac511403ef879
siqqQ <gprex1@yahoo.com>
2018-02-24 23:25:35 +0200

    Add diamonds field, getter and setter in Player class

--- src/Player/Player.java
+++ src/Player/Player.java
@@ -1,5 +1,15 @@
 package Player;
 
 public class Player {
+	private int diamonds = 0;
+	
+	public int getDiamonds() {
+		return diamonds;
+	}
 
+	public void setDiamonds(int diamonds) {
+		this.diamonds = diamonds;
+	}
+
+	
 }
