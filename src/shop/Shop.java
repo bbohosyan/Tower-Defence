84eca8f13ce8b17fdcea4a9d9ae0ab1a4d03a3e2
siqqQ <gprex1@yahoo.com>
2018-02-24 23:08:58 +0200

    Add reduce quantity of reinforcements method and logic

--- src/shop/Shop.java
+++ src/shop/Shop.java
@@ -1,6 +1,13 @@
 package shop;
 
 public class Shop {
+	
+	private short numberOfChillWands = 999;
+	private short numberOfDynamites = 999;
+	private short numberOfFatBoys = 999;
+	private short numberOfGoldBags = 999;
+	private short numberOfHeartboxes = 999;
+	
 	static Shop shop = new Shop();
 	private Shop() {
 
@@ -8,5 +15,28 @@ public class Shop {
 	
 	public static Shop getInstance() {
 		return shop;
+	}
+	
+	public void reduceReinforcementQuantity(String nameOfTheReinforcement) {
+		switch (nameOfTheReinforcement) {
+		case "Chill Wand":
+			numberOfChillWands--;
+			break;
+		case "Dynamite":
+			numberOfDynamites--;
+			break;
+		case "Fat Boy":
+			numberOfFatBoys--;
+			break;
+		case "Gold Bag":
+			numberOfGoldBags--;
+			break;
+		case "Heartbox":
+			numberOfHeartboxes--;
+			break;
+
+		default:
+			break;
+		}
 	}
 }
