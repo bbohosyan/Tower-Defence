def49066fae93310247cdc269c4a9b84d00b8817
siqqQ <gprex1@yahoo.com>
2018-02-25 21:52:52 +0200

    Add main HeroRoom mathods

--- src/hero/HeroRoom.java
+++ src/hero/HeroRoom.java
@@ -1,5 +1,19 @@
 package hero;
 
 public class HeroRoom {
+	
+	private boolean isHeroSelected = false;
+	
+	public HeroRoom() {
 
+	}
+	
+	public void selectHero() {
+		isHeroSelected = true;
+	}
+	
+	public void deselectHero() {
+		isHeroSelected = false;
+	}
+	
 }
